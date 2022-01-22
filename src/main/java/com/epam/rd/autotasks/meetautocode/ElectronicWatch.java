57be17e684f161eb3802af56ade8bafecc56affd
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-22 12:12:54 +0200

    done

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -1,20 +1,16 @@
 package com.epam.rd.autotasks.meetautocode;
 
 import java.util.Scanner;
-import java.time.DateTimeException;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
         try {
-            System.out.println(DateTimeFormatter.ofPattern("H:mm:ss").format(LocalTime.ofSecondOfDay(input % (24 * 60 * 60))));
+            System.out.println(DateTimeFormatter.ofPattern("H:mm:ss").format(LocalTime.ofSecondOfDay(seconds % (24 * 60 * 60))));
         } catch (DateTimeException e) {
             e.printStackTrace();
             System.out.println("Invalid value");
         }
-        System.out.println();
     }
 }
