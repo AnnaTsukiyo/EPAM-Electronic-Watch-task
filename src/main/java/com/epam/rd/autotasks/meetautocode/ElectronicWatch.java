43b21a65386ab3226f7e7d44cdcc469d8e1a07ac
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-23 00:31:49 +0200

    revised version

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,9 +6,16 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-        int h = seconds / 3600;
-        int m = (seconds % 3600) / 60;
-        int s = seconds % 60;
-        System.out.printf("%02d:%02d:%02d", h, m, s);
+        int hours, sec, minutes;
+        if (seconds >= 86400) {
+            hours = 0;
+            minutes = (seconds % 86400) / 60;
+            sec = seconds % 60;
+        } else {
+            hours = seconds / 3600;
+            minutes = seconds % 3600 / 60;
+            sec = seconds % 60;
+        }
+        System.out.printf("%2d:%02d:%02d", hours, minutes, sec);
     }
 }
